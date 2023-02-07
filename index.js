ca92a3577d0ae1f7d29efd538cbd2c22aa1cee0d
Jay Shah <47387734+Jayshah25@users.noreply.github.com>
2023-02-07 17:45:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,43 +15,47 @@ function gameResult(p1, p2){
     }
 
     else{
-        document.getElementsByTagName("h4")[0].textContent = "It was a Draw!";
+        document.getElementsByTagName("h4")[0].textContent = "Draw!";
     }
 }
 
-function updateDice(score){
+function updateDice(score, player){
     var update = {
-        1: [2,3,4,5,6],
-        2: [2,4,5,6],
-        3: [4,5,6],
-        4: [5,6],
-        5: [5],
-        6: false
+        1: [2], // [dotsToAppear]
+        2: [1,3],
+        3: [1,2,3],
+        4: [1,3,4,6],
+        5: [1,2,3,4,6],
+        6: [1,2,3,4,5,6]
     }
 
-    if (update[score]){
-        for (var i in update[score]){
-            document.getElementsByClassName("dot"+i)[0].classList.add("removedDot");
-        }
+    // update dice to initial condition
+    for (var i of [1,2,3,4,5,6]){
+        const list = document.getElementById(player + "dot" + i).classList;
+        list.remove("addDot");
+    }
+
+    // make dots appear with respect to player score
+    for (var i of update[score]){
+        const list = document.getElementById(player + "dot" + i).classList;
+        list.add("addDot")
     }
 }
 
 function playDice() {
 
-    // get random scores for the two players
-    let player1Score = Math.ceil(Math.random() * 6);
-    let player2Score = Math.ceil(Math.random() * 6);
-
-    updateDice(player1Score); // update dice for player 1
-    updateDice(player2Score); // update dice for player 2
-
-    gameResult(player1Score, player2Score); // update the game result
-
     // increment game count
     count+=1;
     document.getElementsByTagName("h3")[0].textContent = "Games Played: " + count;
 
+    // get random scores for the two players in range [1,6]
+    let player1Score = Math.ceil(Math.random() * 6);
+    let player2Score = Math.ceil(Math.random() * 6);
 
+    updateDice(player1Score, 'p1-'); // update dice for player 1
+    updateDice(player2Score,'p2-'); // update dice for player 2
+
+    gameResult(player1Score, player2Score); // update the game result on screen
     
 }
 
@@ -61,18 +65,24 @@ function resetGame(){
     count = 0;
     document.getElementsByTagName("h3")[0].textContent = "Games Played: " + count;
 
-    for (var i in [1,2,3,4,5,6]){
-        document.getElementsByClassName("dot"+i)[0].classList.remove("removedDot");
+    // reset dice
+    for (var i of [1,2,3,4,5,6]){
+        document.getElementById("p1-" + "dot" + i).classList.remove("addDot");
+        document.getElementById("p2-" + "dot" + i).classList.remove("addDot");
     }
 
+    // display the session result
     alert(`
-    Player 1 Score: ${player1}
-    Player 2 Score: ${player2}
+    Player 1 wins ${player1} Games!
+    Player 2 wins ${player2} Games!
     `);
 
     // Reset the Player Scores
     player1 = 0;
     player2 = 0;
+
+    // Update the result board
+    document.getElementsByTagName("h4")[0].textContent = "Game About to Start!";
 
 }
 
